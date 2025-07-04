bd2a2d0e18eec02e12714756a608995de7a2ea14
alberto <albertopuet8@gmail.com>
2025-07-04 02:34:21 -0300

    primer commit

--- src/app/crear-producto/page.tsx
+++ src/app/crear-producto/page.tsx
@@ -5,64 +5,51 @@ import React, { useState } from "react";
 export default function About() {
   const [product, setProduct] = useState({ name: "", price: "" });
 
-
-  const handleChange = (e: { target: { value: any; name: any; }; }) => {
-    const inputValue = e.target.value;
-    const inputName = e.target.name;
-
-    setProduct({
-      ...product,
-      [inputName] : inputValue
-    })
-    
-    
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProduct((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault(); 
-  if (!product.name || !product.price) {
-    alert('Ambos campos son obligatorios')
-  }
-
-  try {
-    await axios.post("http://localhost:5000/api/v1/products", product);
-    setProduct({ name: "", price: "" });
-    console.log('enviado');
-    
-  } catch (error) {
-    console.error("Error al crear producto:", error);
-  }
-};
-
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/api/v1/products", product);
+      setProduct({ name: "", price: "" });
+    } catch (error) {
+      console.error("Error al crear producto:", error);
+    }
+  };
 
   return (
     <div className="h-screen bg-black flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Crear Producto</h1>
       <form
-  onSubmit={handleSubmit}
-  className="flex flex-col min-w-[50%] gap-5 p-10 bg-slate-600/80 rounded-xl my-10"
->
-  <input
-    type="text"
-    placeholder="Nombre del producto"
-    name="name"
-    className="border rounded-sm p-2"
-    onChange={handleChange}
-    value={product.name}
-  />
-  <input
-    type="number"
-    placeholder="Precio"
-    name="price"
-    className="border rounded-sm p-2"
-    onChange={handleChange}
-    value={product.price}
-  />
-  <button type="submit" className="bg-amber-400 rounded-sm p-1">
-    Añadir producto
-  </button>
-</form>
-
+        onSubmit={handleSubmit}
+        className="flex flex-col min-w-[50%] gap-5 p-10 bg-slate-600/80 rounded-xl my-10"
+      >
+        <input
+          type="text"
+          placeholder="Nombre del producto"
+          name="name"
+          className="border rounded-sm p-2"
+          onChange={handleChange}
+          value={product.name}
+        />
+        <input
+          type="number"
+          placeholder="Precio"
+          name="price"
+          className="border rounded-sm p-2"
+          onChange={handleChange}
+          value={product.price}
+        />
+        <button type="submit" className="bg-amber-400 rounded-sm p-1">
+          Añadir producto
+        </button>
+      </form>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 >
         Este es el Home
       </h1>
-      {/* <Link href='/crear-producto' className='bg-amber-400 p-1 rounded-sm my-10'>{`Crear Producto >>>`}</Link> */}
+      <Link href='/crear-producto' className='bg-amber-400 p-1 rounded-sm my-10'>{`Crear Producto >>>`}</Link>
     </div>
   )
 }
